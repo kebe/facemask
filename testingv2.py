b4688f6634b463327919acd30b76a0d464f8f268
Ousmane Kebe <ousmane.kebe@gmail.com>
2021-03-04 14:22:24 -0500

    2 classes

---
+++ testingv2.py
@@ -0,0 +1,66 @@
+# Python program to test network on a dataset saved in the same format as the CIFAR batches  
+
+#importing files for a basic NN setup
+from __future__ import print_function
+import torch
+import numpy as np
+import sys
+from statistics import mean
+from torchvision import transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5) 
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 53 * 53, 1200)  #sqrt(179776/4/16)
+        self.fc2 = nn.Linear(1200, 300)
+        self.fc3 = nn.Linear(300, 3)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        #import pdb; pdb.set_trace()
+        x = x.view(x.size(0), 16 * 53 * 53)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+    def test(input_tensor, output_tensor, PATH):
+       
+        
+        net = Net()
+        net.load_state_dict(torch.load(PATH))
+
+        out = net(input_tensor)
+        accuracy = output_tensor.eq(out.detach().argmax(dim=1)).float().mean()
+        print('accuracy:', accuracy.item())
+
+        print(output_tensor)
+        print(out.detach().argmax(dim=1))
+
+        #import pdb; pdb.set_trace()
+        image = input_tensor[44];npimg = image.numpy();plt.imshow(np.transpose(npimg, (1, 2, 0)));plt.show()
+        image = input_tensor[45];npimg = image.numpy();plt.imshow(np.transpose(npimg, (1, 2, 0)));plt.show()
+        image = input_tensor[46];npimg = image.numpy();plt.imshow(np.transpose(npimg, (1, 2, 0)));plt.show()
+
+
+
+#Test Harness main method
+if __name__ == "__main__":
+
+    # # CIFAR BATCHES
+    #Input arguments: network file name and data file name
+    PATH = "outputv2_840/epoch_18_trained.pth"
+  
+    test_input_tensor = torch.load('tensor_datav2_840/test_images_tensor.pt')
+    test_output_tensor = torch.load('tensor_datav2_840/test_labels_tensor.pt')
+
+    Net.test(test_input_tensor, test_output_tensor, PATH)
---
+++ trainingv2.py
@@ -0,0 +1,303 @@
+from __future__ import print_function
+
+import numpy as np # linear algebra
+import matplotlib.pyplot as plt
+#matplotlib inline
+import matplotlib.patches as patches
+import os
+from PIL import Image
+import cv2
+import warnings
+import xmltodict
+warnings.filterwarnings("ignore")
+
+import torch
+from torchvision import datasets,transforms,models
+from torch.utils.data import Dataset,DataLoader
+
+
+import sys
+from statistics import mean
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from datetime import datetime
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5) #in_channels: int, out_channels: int, kernel_size
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 53 * 53, 1200)  #sqrt(179776/4/16)
+        self.fc2 = nn.Linear(1200, 300)
+        self.fc3 = nn.Linear(300, 3)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        #import pdb; pdb.set_trace()
+        x = x.view(x.size(0), 16 * 53 * 53)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+  
+    def train(dataset):
+       
+        train_size=int(len(dataset)*0.7) # 70% of data for training
+        val_size=int(len(dataset)*0.2)   # 20% of data for validation
+        test_size=len(dataset)-train_size-val_size # 10% of data for testing
+        batch_size=4
+        trainset,valset,testset=torch.utils.data.random_split(dataset,[train_size,val_size,test_size]) #split data into the 3 sets
+
+        # turn tuples into dataloader objects
+        train_loader =DataLoader(dataset=trainset,batch_size=len(trainset),shuffle=True)
+        val_loader =DataLoader(dataset=valset,batch_size=len(valset),shuffle=True)
+        test_loader =DataLoader(dataset=testset,batch_size=len(testset),shuffle=True)
+
+        dataiter=iter(train_loader) 
+        train_images_tensor,train_labels_tensor=dataiter.next()
+
+        val_dataiter=iter(val_loader) 
+        val_images_tensor,val_labels_tensor=val_dataiter.next()
+
+        test_dataiter=iter(test_loader) 
+        test_images_tensor,test_labels_tensor=test_dataiter.next()
+
+        
+        torch.save(train_images_tensor, 'tensor_datav2_840/train_images_tensor.pt')
+        torch.save(train_labels_tensor, 'tensor_datav2_840/train_labels_tensor.pt')
+        torch.save(val_images_tensor, 'tensor_datav2_840/val_images_tensor.pt')
+        torch.save(val_labels_tensor, 'tensor_datav2_840/val_labels_tensor.pt')
+        torch.save(test_images_tensor, 'tensor_datav2_840/test_images_tensor.pt')
+        torch.save(test_labels_tensor, 'tensor_datav2_840/test_labels_tensor.pt')
+
+        # train_images_tensor = torch.load('tensor_datav2/train_images_tensor.pt')
+        # train_labels_tensor = torch.load('tensor_datav2/train_labels_tensor.pt')
+        # val_images_tensor = torch.load('tensor_datav2/val_images_tensor.pt')
+        # val_labels_tensor = torch.load('tensor_datav2/val_labels_tensor.pt')
+        # test_images_tensor = torch.load('tensor_datav2/test_images_tensor.pt')
+        # test_labels_tensor = torch.load('tensor_datav2/test_labels_tensor.pt')
+        #import pdb; pdb.set_trace()
+        net = Net()
+
+        criterionCE = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5)
+
+        # empty list to store training losses
+        train_losses = []
+        # empty list to store validation losses
+        val_losses = []
+        train_accuracies = list()
+        val_accuracies = list()
+        batch_size = 4
+        epoch_stop_train = 200 #default stop point if training goes all the way
+        dir_path = "outputv2_840"
+        os.makedirs(dir_path, exist_ok=True) 
+
+        #minibatch calculations
+        num_of_batches = train_images_tensor.size()[0] // batch_size
+        if train_images_tensor.size()[0] % batch_size > 0 :
+            last_batch = train_images_tensor.size()[0] % batch_size 
+        else:
+            last_batch = 0
+        
+        for epoch in range(100):  # loop over the dataset multiple times
+
+            print(epoch)
+            i =0
+            sub_train_accuracies = list()
+            while i <= num_of_batches:
+                start = (i* batch_size)
+                end = (i* batch_size) + batch_size
+
+                #case of last batch
+                if i == num_of_batches:
+                    if last_batch:
+                        start = (i* batch_size) 
+                        end = (i* batch_size) + last_batch
+                    else:
+                        break
+
+                input_tensor_batch = train_images_tensor[start:end]
+                output_tensor_batch = train_labels_tensor[start:end]
+                
+                i +=1
+                #import pdb; pdb.set_trace()
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                out_training = net(input_tensor_batch)
+                loss_train = criterionCE(out_training, output_tensor_batch)
+                
+                #append losses
+                if i == 1:
+                    train_losses.append(loss_train.detach().numpy().mean())
+
+                loss_train.backward()
+                optimizer.step()
+                #tr_loss = loss_train.item()
+                sub_train_accuracies.append(output_tensor_batch.eq(out_training.detach().argmax(dim=1)).float().mean())
+                #import pdb; pdb.set_trace()
+
+            train_accuracies.append(sum(sub_train_accuracies) / len(sub_train_accuracies) )
+            #validate data    
+            out_validation = net(val_images_tensor)
+            loss_validation = criterionCE(out_validation, val_labels_tensor)
+            val_losses.append(loss_validation.detach().numpy().mean())
+            val_accuracies.append(val_labels_tensor.eq(out_validation.detach().argmax(dim=1)).float().mean())
+
+            #save networks 
+            PATH = "./" + dir_path +"/epoch_" + str(epoch) +"_trained.pth"
+            torch.save(net.state_dict(), PATH)
+
+            # printing the validation loss
+            print('Epoch : ',epoch+1, '\t', 'val loss :', loss_validation)
+            print('Epoch : ',epoch+1, '\t', 'train loss :', loss_train)
+            print('Epoch : ',epoch+1, '\t', 'val accuracy :', torch.tensor(val_accuracies).mean())
+            print('Epoch : ',epoch+1, '\t', 'train accuracy :', torch.tensor(train_accuracies).mean())
+
+            #if validation error goes up for 4 epochs in a row 
+            val_size = len(val_losses)
+            # if epoch >10 and val_losses[val_size-1] > val_losses[val_size-2] > val_losses[val_size-3] > val_losses[val_size-4] > val_losses[val_size-5]:
+            #     epoch_stop_train = epoch-4
+            #     break
+
+        plt.plot(train_losses, label='Training loss')
+        plt.plot(val_losses, label='Validation loss')
+        plt.plot(train_accuracies, label='Training accuracies')
+        plt.plot(val_accuracies, label='Validation accuracies')
+        plt.legend()
+        plt.savefig("./" + dir_path +"/size_"+ "_plot.png")
+        print('Finished Training at epoch:',epoch_stop_train )
+
+
+
+
+
+#function to make dataset
+def make_dataset(no_of_images): 
+    image_tensor=[]
+    label_tensor=[]
+    with_mask_count =0
+    without_mask_count =0
+    total_size =840
+    #count =0
+    for i,j in enumerate(no_of_images):
+        # count = count +1
+        # if count > 512:
+        #     break
+        with open(path_annotations+j[:-4]+".xml") as fd:
+            doc=xmltodict.parse(fd.read())
+        if type(doc["annotation"]["object"])!=list:
+            temp=doc["annotation"]["object"]
+            a,b,c,d=list(map(int,temp["bndbox"].values()))
+            label=options[temp["name"]]
+            if label == 0 and with_mask_count < total_size :
+                with_mask_count +=1
+                print('with_mask_count:', with_mask_count)
+                image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                image_tensor.append(my_transform(image))
+                label_tensor.append(torch.tensor(label))                
+            if label == 1 and without_mask_count < total_size :
+                without_mask_count +=1
+                print('without_mask_count:', without_mask_count)
+                image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                image_tensor.append(my_transform(image))
+                label_tensor.append(torch.tensor(label))
+            if label == 2 and without_mask_count < total_size:
+                without_mask_count +=1
+                print('without_mask_count:', without_mask_count)
+                image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                image_tensor.append(my_transform(image))
+                label_tensor.append(torch.tensor(1))
+        else:
+            temp=doc["annotation"]["object"]
+            for k in range(len(temp)):
+                a,b,c,d=list(map(int,temp[k]["bndbox"].values()))
+                label=options[temp[k]["name"]]
+                if label == 0 and with_mask_count < total_size :
+                    with_mask_count +=1
+                    print('with_mask_count:', with_mask_count)
+                    image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                    image_tensor.append(my_transform(image))
+                    label_tensor.append(torch.tensor(label))                
+                if label == 1 and without_mask_count < total_size :
+                    without_mask_count +=1
+                    print('without_mask_count:', without_mask_count)
+                    image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                    image_tensor.append(my_transform(image))
+                    label_tensor.append(torch.tensor(label))
+                if label == 2 and without_mask_count < total_size:
+                    without_mask_count +=1
+                    print('without_mask_count:', without_mask_count)
+                    image=transforms.functional.crop(Image.open(path_images+j).convert("RGB"), b,a,d-b,c-a)
+                    image_tensor.append(my_transform(image))
+                    label_tensor.append(torch.tensor(1))
+                
+    final_dataset=[[k,l] for k,l in zip(image_tensor,label_tensor)]
+    return tuple(final_dataset)
+
+if __name__ == "__main__":
+
+    path_images="images/"  #path for an image folder 
+    path_annotations="annotations/" #path for xmlfiles folder
+
+    imagenames=[] #list of imagefile names
+    xmlnames=[] #list of xmlfile names
+    # Put images ans xml files into arrays
+    for dirname, _, filenames in os.walk(path_annotations):
+        for filename in filenames:
+            xmlnames.append(filename)
+
+    for dirname, _, filenames in os.walk(path_images):
+        for filename in filenames:
+            imagenames.append(filename)
+
+    ## Code for finding the total no of labels in our dataset
+    listing=[]
+    for i in imagenames[:]:
+        with open(path_annotations+i[:-4]+".xml") as fd:
+            doc=xmltodict.parse(fd.read())
+        temp=doc["annotation"]["object"]
+        if type(temp)==list:
+            for i in range(len(temp)):
+                listing.append(temp[i]["name"])
+        else:
+            listing.append(temp["name"])
+
+    for i in  set(listing):
+        print(i)
+
+    # mapping for predictions and analysis purpose
+    options={"with_mask":0,"without_mask":1,"mask_weared_incorrect":1} 
+
+    # for i in range(0,5):
+    #     draw_bounding_box(imagenames[i])
+
+
+    #importing neccessary libraries for deeplearning task..
+
+    my_transform=transforms.Compose([transforms.Resize((226,226)),
+                                    transforms.ToTensor()])
+
+    #dataset = []
+    dataset=make_dataset(imagenames) #making a datset
+
+
+
+    now = datetime.now()
+    start_time = now.strftime("%H:%M:%S")
+    print("Start Time =", start_time)
+
+    Net.train(dataset)
+
+    now = datetime.now()
+    end_time = now.strftime("%H:%M:%S")
+    print("Start Time =", start_time)
+    print("End Time =", end_time)
